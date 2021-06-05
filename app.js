18dad81de64780e727906f41c50a3dcb3ccba035
Daliz Cruz <dalizcruz@gmail.com>
2021-06-05 12:02:07 -0500

    bonus test

--- app.js
+++ app.js
@@ -1,70 +1,69 @@
 const express = require("express");
-const app = express()
+const app = express();
 const PORT = 3002;
-app.listen(PORT, () => { console.log("listening on PORT: ", PORT)});
-
+app.listen(PORT, () => {
+  console.log("listening on PORT: ", PORT);
+});
 
 const magic8Responses = [
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes - Definitely",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes, and signs point to yes",
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful"
-  ]
-
-  
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
 
 app.get("/terminator", (req, res) => {
-    res.send("I'll be back")
-    res.send("Hasta la vista, baby")//this message doesnt appear, only the above msg
-
-})
+  res.send("I'll be back");
+  res.send("Hasta la vista, baby"); //this message doesnt appear, only the above msg
+});
 app.get("/Emeril", (req, res) => {
-    res.send("Bam!")
-})
+  res.send("Bam!");
+});
 app.get("/Tony-Montana", (req, res) => {
-    res.send("Say hello to my little friend")
-})
+  res.send("Say hello to my little friend");
+});
 app.get("/Homer-Simpson", (req, res) => {
-    res.send(`D'Oh`)
-})
+  res.send(`D'Oh`);
+});
 app.get("/JJ-Evans", (req, res) => {
-    res.send("Dy-no-myte!")
-})
+  res.send("Dy-no-myte!");
+});
 app.get("/Frankenstein", (req, res) => {
-    res.send("It's alive! It's alive")
-})
+  res.send("It's alive! It's alive");
+});
 app.get("/Gollum", (req, res) => {
-    res.send("My precious")
-})
+  res.send("My precious");
+});
 app.get("/Rocky", (req, res) => {
-    res.send("Yo, Adrian")
-})
+  res.send("Yo, Adrian");
+});
 app.get("/ric-flair", (req, res) => {
-    res.send("Whoooooooo")
-})
+  res.send("Whoooooooo");
+});
 app.get("/james-bond", (req, res) => {
-    res.send("the name is Bond, James Bond")
-})
+  res.send("the name is Bond, James Bond");
+});
 app.get("/Regis", (req, res) => {
-    res.send("Is that your final answer?")
-})
+  res.send("Is that your final answer?");
+});
 
 app.get("/Magic8Ball", (req, res) => {
-    const randomRes = magic8Responses[Math.floor(Math.random() * magic8Responses.length)]
-    res.send(`<h1>${randomRes}</h1>`)
-})
+  const randomRes =
+    magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
+  res.send(`<h1>${randomRes}</h1>`);
+});
---
+++ tests/app.test.js
@@ -0,0 +1,9 @@
+const supertest = require('supertest')
+// const app = require('./tests/app.test.js')
+
+
+
+ it('Testing to see if supertest works', ()=> {
+   expect(1).toBe(1)
+ })
+
