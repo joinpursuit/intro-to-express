086cd5300c3cd872c6cd0d12a11514a6585d7993
Durdona Djalilova <ddjalilova3@gmail.com>
2021-06-05 12:53:35 -0400

    lab done

--- app.js
+++ app.js
@@ -47,6 +47,35 @@ app.get("/fraiser", (request, response) => {
     response.send("I'm listening")
 })
 
-app.listen(PORT, () => {
-    console.log("Listening on PORT: ", PORT)
-})
+app.listen(PORT, () => {})
+
+
+
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+    
+]
+
+
+app.get("/magic8", (req, res)=>{
+    let randomResponse = magic8Responses[Math.floor(Math.random() * magic8Responses.length)]
+        res.send(`<h1>${randomResponse}</h1>`)
+    })
