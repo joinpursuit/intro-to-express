5a9539fd1fefc0b41444e7b55f563c85a3e41fe3
Cari <t.carina@rocketmail.com>
2021-06-05 11:49:10 -0400

    catchprhases

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 3003
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT ', PORT)
+})
+
+app.get('/emeril', (req, res) => {
+    res.send("Bam!")
+})
+
+app.get('/stevemcgarrett', (req, res) => {
+    res.send('Book em Danno!')
+})
+
+app.get('/rocky', (req, res) => {
+    res.send('Yo, Adrian')
+})
+
+app.get('/zeus', (req, res) => {
+    res.send('Release the Kraken')
+})
+
+app.get('/tonymontana', (req, res) => {
+    res.send('Say hello to my little friend')
+})
+
+app.get('/dorothy', (req, res) => {
+    res.send('Toto, Ive got a feeling were not in Kansas anymore')
+})
+
+app.get('/jjevans', (req,res) => {
+    res.send('Dy-no-myte!')
+})
+
+app.get('/batman', (req, res) => {
+    app.send('To the Batmobile!')
+})
+
+app.get('/gollum', (req, res) => {
+    app.send('My precious')
+})
+
