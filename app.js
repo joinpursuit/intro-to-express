2f20dca1b36335edacab6af92a917067754436f9
Daliz Cruz <dalizcruz@gmail.com>
2021-06-05 11:31:39 -0500

    magic 8 ball

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ const app = express()
 const PORT = 3002;
 app.listen(PORT, () => { console.log("listening on PORT: ", PORT)});
 
+
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ]
+
+  
+
 app.get("/terminator", (req, res) => {
     res.send("I'll be back")
+    res.send("Hasta la vista, baby")//this message doesnt appear, only the above msg
+
 })
 app.get("/Emeril", (req, res) => {
     res.send("Bam!")
@@ -35,4 +62,9 @@ app.get("/james-bond", (req, res) => {
 })
 app.get("/Regis", (req, res) => {
     res.send("Is that your final answer?")
+})
+
+app.get("/Magic8Ball", (req, res) => {
+    const randomRes = magic8Responses[Math.floor(Math.random() * magic8Responses.length)]
+    res.send(`<h1>${randomRes}</h1>`)
 })
