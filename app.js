64e4af94735438a7f429f458ed9d744339781879
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-06-07 15:58:09 -0400

    done

---
+++ app.js
@@ -0,0 +1,68 @@
+const { request } = require("express");
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("I love express!");
+});
+app.get("/terminator", (request, response) => {
+  response.send("I'll be back");
+});
+app.get("/terminator2", (request, response) => {
+  response.send("Hasta la vista, baby");
+});
+app.get("/jjevans", (request, response) => {
+  response.send("Dy-no-myte!!");
+});
+app.get("/batman", (request, response) => {
+  response.send("To the Batmobile!");
+});
+app.get("/HannibalSmith", (request, response) => {
+  response.send("I love it when a plan comes together");
+});
+app.get("/Zeus", (request, response) => {
+  response.send("Release the Kraken");
+});
+app.get("/JamesBond", (request, response) => {
+  response.send("the name is Bond, James Bond");
+});
+app.get("/Frankenstein", (request, response) => {
+  response.send("It's alive! It's alive");
+});
+app.get("/Rocky", (request, response) => {
+  response.send("Yo, Adrian");
+});
+app.get("/Gollum", (request, response) => {
+  response.send("My precious");
+});
+app.get("/JackDawson", (request, response) => {
+  response.send("I'm king of the world!");
+});
+const magic8Responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+app.get("/magic8", (request, response) => {
+  let random =
+    magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
+  response.send(`<h1>${random}</h1>`);
+
+  response.send(`<h1>${magic8Responses[random]}</h1>`);
+});
