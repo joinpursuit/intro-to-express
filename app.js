e5287b64c9c444b51afd3b157082b4741c8deb14
Sara Perkaj <saraperkaj@pursuit.org>
2022-01-05 13:15:48 -0500

    Finished routes to ten pages.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
@@ -7,6 +8,46 @@ const PORT = 3003;
 //then do this function
 app.get("/", (request, response) => {
   response.send("Yellow World");
+});
+
+app.get("/terminator", (request, response) => {
+  response.send("Make it work.");
+});
+
+app.get("/bruce-banner", (request, response) => {
+  response.send("Don't make me angry.");
+});
+
+app.get("/borg", (request, response) => {
+  response.send("Resistance is futile.");
+});
+
+app.get("/zeus", (request, response) => {
+  response.send("Release the Kraken.");
+});
+
+app.get("/rocky", (request, response) => {
+  response.send("Yo, Adrian");
+});
+
+app.get("/gollum", (request, response) => {
+  response.send("My precious");
+});
+
+app.get("/james-bond", (request, response) => {
+  response.send("The name is Bond, James Bond.");
+});
+
+app.get("/Jim-Lovell", (request, response) => {
+  response.send("Houston, we have a problem.");
+});
+
+app.get("/Jack_Dawson", (request, response) => {
+  response.send("I'm King of the world!");
+});
+
+app.get("/Homer-Simpson", (request, response) => {
+  response.send("D'Oh");
 });
 
 app.listen(PORT, () => {
