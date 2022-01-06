3c7d0a7b39c2d49c78485906c82d800b60a788ec
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-06 13:51:16 -0500

    completed Activity - Magic 8 Ball

--- app.js
+++ app.js
@@ -91,6 +91,37 @@ app.get('/dorothy', (request, response)=>{
 })
 
 
+/* 
+    Activity - Magic 8 Ball: 
+*/
+app.get('/magic8', (request, response)=>{
+    const magic8Responses = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes - Definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes, and signs point to yes",
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+      ]
+
+     const randomPhrase = magic8Responses[Math.floor(Math.random()*magic8Responses.length)];
+
+    response.send(randomPhrase);
+})
+
 // Listen
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
