6bce4ca1f2ee24ae13cfc95b46dc082a37059a28
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-06 00:44:00 -0500

    added a new route /magic8 that randomly returns a catch phrase from the array of

--- app.js
+++ app.js
@@ -66,3 +66,31 @@ app.get("/fraiser", (request, response) => {
 })
 
 // Activity Magic 8 Ball
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+]
+
+app.get("/magic8", (request, response) => {
+    const getRandom = (arr) => {
+        return arr[Math.floor(Math.random() * arr.length)]
+    }
+    response.send(getRandom(magic8Responses))
+})
