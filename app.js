cf8c07511876cd1ad03bfb4e0010ee1c45dd6298
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-01-06 16:14:14 -0500

    added more test for fun and learning

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require("express");
+const magic8Responses = require("./data.js");
 
 // CONFIGURATION
 const app = express();
@@ -48,34 +49,7 @@ app.get("/rocky", (request, response) => {
 });
 
 app.get("/magic8", (request, response) => {
-
-    const magic8Responses = [
-        "It is certain",
-        "It is decidedly so",
-        "Without a doubt",
-        "Yes - Definitely",
-        "You may rely on it",
-        "As I see it, yes",
-        "Most likely",
-        "Outlook good",
-        "Yes, and signs point to yes",
-        "Reply hazy, try again",
-        "Ask again later",
-        "Better not tell you now",
-        "Cannot predict now",
-        "Concentrate and ask again",
-        "Don't count on it",
-        "My reply is no",
-        "My sources say no",
-        "Outlook not so good",
-        "Very doubtful"
-    
-    ];
-
     const magic8Answer = magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
-
-    console.log(magic8Answer);
-
     response.send(`<h1>${magic8Answer}</h1>`);
 })
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,17 +1,119 @@
 // DEPENDENCIES
 const app = require("../app.js")
 const request = require("supertest")
+const magic8Responses = require("../data.js");
 
     // it("Testing to see if supertest works", () => {
     //     expect(1).toBe(2)
     // })
 
     describe("Test the root path", () => {
-        test("It should response the GET method", () => {
+        test("It should response the GET method and display a dog or cat status image of 418", () => {
             return request(app)
                 .get("/")
                 .then(res => {
                     expect(res.statusCode).toBe(418);
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Terminator quote", () => {
+            return request(app)
+                .get("/terminator")
+                .then(res => {
+                    expect(res.text).toBe("I'll be back & Hasta la vista, baby");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Batman quote", () => {
+            return request(app)
+                .get("/batman")
+                .then(res => {
+                    expect(res.text).toBe("To the Batmobile!");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Homer Simpson quote", () => {
+            return request(app)
+                .get("/homer-simpson")
+                .then(res => {
+                    expect(res.text).toBe("D'Oh");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Bruce Banner quote", () => {
+            return request(app)
+                .get("/bruce-banner")
+                .then(res => {
+                    expect(res.text).toBe("Don't make me angry");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Tony Montana quote", () => {
+            return request(app)
+                .get("/tony-montana")
+                .then(res => {
+                    expect(res.text).toBe("Say hello to my little friend");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Zeus quote", () => {
+            return request(app)
+                .get("/zeus")
+                .then(res => {
+                    expect(res.text).toBe("Release the Kraken");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the James Bond quote", () => {
+            return request(app)
+                .get("/james-bond")
+                .then(res => {
+                    expect(res.text).toBe("the name is Bond, James Bond");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Frankenstein quote", () => {
+            return request(app)
+                .get("/frankenstein")
+                .then(res => {
+                    expect(res.text).toBe("It's alive! It's alive");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        test("It should response the Rocky Balboa quote", () => {
+            return request(app)
+                .get("/rocky")
+                .then(res => {
+                    expect(res.text).toBe("Yo, Adrian");
+                })
+        })
+    })
+
+    describe("Test the root path", () => {
+        const magic8Answer = magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
+        test("It should response Mutiple dynamic quotes", () => {
+            return request(app)
+                .get("/magic8")
+                .then(res => {
+                    expect(res.statusCode).toBe(200);
                 })
         })
     })
