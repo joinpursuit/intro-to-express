5320b2b9e5d8934cbb9a77918c0db874a2053989
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-01-06 12:10:21 -0500

    first commit, Terminator & Magic8 responses

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+let app = express();
+
+app.get("/terminator", (req, res)=>{
+    res.send("Hasta la vista, baby");
+})
+
+app.get("/tim-gunn", (req, res) => {
+    res.send("Make it work")
+})
+
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  
+  ]
+
+  const getResponses = () => {
+    let randomPhrase = magic8Responses[Math.floor(Math.random()*magic8Responses.length)];
+    return randomPhrase;
+      }
+
+app.get("/magic8", (req, res) =>{
+
+    res.send(getResponses());
+})
+
+app.listen(3003);
