f97fc5dbd46d78789a377f3be5c033a39083a9df
David Vidals <david.vidals08@gmail.com>
2022-01-18 01:56:46 -0500

    fully completed lab

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
+const { response } = require('express')
 const express = require('express')
+const responses = require('./responses')
 
 const app=express()
 const PORT = 3003
 
 app.get('/', (req, res)=>{
     res.send('Hello World!')
+})
+
+app.get('/terminator', (req, res) => {
+    res.send(`I'll be back`)
+  })
+
+app.get('/terminator2', (req, res) => {
+    res.send(`Hasta la vista, baby`)
+  })
+
+app.get('/Emeril',(req,res)=>{
+    res.send('Bam!')
+})
+
+app.get('/homer-simpson',(req,res)=>{
+    res.send(`D'oh`)
+})
+app.get('/Bad-bunny',(req,res)=>{
+    res.send('yeah yeah yeah yeah')
+})
+app.get('/anuel',(req,res)=>{
+    res.send('BRRRRRR')
+})
+app.get('/bruce-banner',(req,res)=>{
+    res.send(`Don't make me angry`)
+})
+app.get('/gollum',(req,res)=>{
+    res.send('My precious')
+})
+app.get('/groot',(req,res)=>{
+    res.send('I AM GROOT')
+})
+app.get('/frankenstein',(req,res)=>{
+    res.send(`It's alive! It's alive.`)
+})
+app.get('/james-bond',(req,res)=>{
+    res.send('the name is Bond, James Bond')
+})
+app.get('/rocky',(req,res)=>{
+    res.send('Yo, Adrian')
+})
+
+
+app.get('/magic8',(req,res)=>{
+  let index = Math.floor(Math.random() * responses.length);
+  let answer = responses[index];
+  res.send(answer)
 })
 
 app.listen(PORT, ()=>{
---
+++ responses.js
@@ -0,0 +1,26 @@
+const { response } = require("express")
+
+const magic8Responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+
+]
+
+module.exports = magic8Responses;
