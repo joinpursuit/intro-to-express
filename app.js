b623178fa5c3e86ba8ab28eef927cd36531f7389
Andres Varon <anvaron@gmail.com>
2022-07-13 17:08:15 -0500

    Intro-to-express lab - Final release

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ const magic8Responses = [
 ]
 
 /**
- * [getIndex() function generates a ramdom index between 0 and array length]
+ * [getIndex() function generates a random index between 0 and array length]
  * @param  {[number]} min [min value equals to the first position]
  * @param  {[number]} max [max value equals to the last position]
- * @return {[number]}     [Number that represents a ramdon index]
+ * @return {[number]}     [Number that represents a random index]
  */
 function getIndex(min, max) {
   min = Math.ceil(min);
@@ -41,25 +41,26 @@ function getIndex(min, max) {
 
 // Routes
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!')
+  res.status(200)
+  .send('<h1 style="padding: 12rem;margin: 10vh auto;text-align: center;font-size: 8rem;text-decoration: underline;background-color:#0081a7;color: #FFF">Hello, world!</h1>')
 })
 
 // Route validating params with logic
 app.get('/terminator:id', (req, res) => {
   if (req.params.id === '1') {
-    res.send('I\ll be back') 
+    res.send('<h4 style="padding: 12rem;margin: 10vh auto;text-align: center;font-size: 4rem;background-color:#3a86ff;color: #FFF">I\ll be back</h4>') 
   }
   if (req.params.id === '2') {
-    res.send('Hasta la vista, baby')
+    res.send('<h4 style="padding: 12rem;margin: 10vh auto;text-align: center;font-size: 4rem;background-color:#ff006e;color: #FFF;">Hasta la vista, baby</h4>')
   }
 })
 
 app.get('/emeril', (req, res) => {
-  res.send('Bam!')
+  res.send('<p style="padding: 1rem;margin: 33vh auto;display: table;text-align: center;font-size: 4rem;font-weight: bold;color: #f07167;border: 1rem solid #f07167;">Bam!</p>')
 })
 
 app.get('/magic8', (req, res) => {
-  res.send(`<h1>${magic8Responses[getIndex(0, magic8Responses.length-1)]}</h1>`)
+  res.send(`<h1 style="padding: 12rem;margin: 10vh auto;text-align: center;font-size: 4rem;background-color:#118ab2;color: #ffd166;">${magic8Responses[getIndex(0, magic8Responses.length-1)]}</h1>`)
 })
 
 // Export
