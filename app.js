6687a9c0ffae1b9a79a0275b21b7e39d1f848599
arupay <arupay11374@gmail.com>
2022-07-13 10:21:56 -0400

    magic 8 addition

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 //DEPENDENCIES
-const { response } = require("express");
 const express = require("express");
 
 //CONFIG
 
 const app = express();
 const PORT = 3003;
+
+const magic8Responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+const random = magic8Responses[Math.floor(Math.random() * 19)];
 
 //ROUTES
 
@@ -33,8 +56,27 @@ app.get("/nightmareonelm", (req, res) => {
   res.send("Welcome to Prime Time, $!@#*");
 });
 
+app.get("/chucky", (req, res) => {
+  res.send("Hi, Im chucky wanna play!");
+});
 app.get("/scarymovie", (req, res) => {
   res.send("Waaaazaaaaaaaaaaaa");
+});
+
+app.get("/jaws", (req, res) => {
+  res.send("Youre gonna needa  bigger boat");
+});
+
+app.get("/frankenstein", (req, res) => {
+  res.send("Its alive! its alive!!");
+});
+
+app.get("/dracula", (req, res) => {
+  res.send("I never drink..... Wine.");
+});
+
+app.get("/magic8", (req, res) => {
+  res.send(`<h1>${random}</h1>`);
 });
 
 app.listen(PORT, () => {
